121176bebdf949f5721e47b581928ac7b910067f
yweijin <yweijin@linewell.com>
2022-08-26 11:22:57 +0800

    feat: 初始化工程

---
+++ eureka-service/src/main/java/xyz/ibudai/CloudServiceApplication.java
@@ -0,0 +1,15 @@
+package xyz.ibudai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class CloudServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudServiceApplication.class, args);
+    }
+
+}
