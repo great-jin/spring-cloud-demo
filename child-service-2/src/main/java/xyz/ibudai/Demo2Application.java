94589f93f84ef07a0e925991f3465683df6bdd08
yweijin <yweijin@linewell.com>
2022-09-01 10:48:38 +0800

    style: 工程规范格式化

--- child-service-2/src/main/java/xyz/ibudai/Demo2Application.java
+++ child-service-2/src/main/java/xyz/ibudai/Demo2Application.java
@@ -2,8 +2,10 @@ package xyz.ibudai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Demo2Application {
 
 	public static void main(String[] args) {
