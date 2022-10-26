2ff9482c516a82b51ab9c4b33e43902e1a345624
great <great__jin@163.com>
2022-10-26 17:31:55 +0800

    fix: 工程模块归档整理

--- eureka-config/config-client/src/main/java/xyz/ibudai/ConfigClientApplication.java
+++ eureka-config/config-client/src/main/java/xyz/ibudai/ConfigClientApplication.java
@@ -7,10 +7,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 // 激活 Eureka 中的 DiscoveryClient 实现
 @EnableDiscoveryClient
-public class ConfigGitApplication {
+public class ConfigClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigGitApplication.class, args);
+		SpringApplication.run(ConfigClientApplication.class, args);
 	}
 
 }
--- eureka-consumer/consumer-ribbon/src/main/java/xyz/ibudai/ConsumerRibbonApplication.java
+++ eureka-consumer/consumer-ribbon/src/main/java/xyz/ibudai/ConsumerRibbonApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ConsumerBasicApplication {
+public class ConsumerRibbonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConsumerBasicApplication.class, args);
+		SpringApplication.run(ConsumerRibbonApplication.class, args);
 	}
 
 }
