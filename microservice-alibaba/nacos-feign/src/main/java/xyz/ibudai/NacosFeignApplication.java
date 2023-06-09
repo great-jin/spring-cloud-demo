9f2570223e827f170f9ac8ddf1147c83b861de97
great <great__jin@163.com>
2023-06-09 11:31:39 +0800

    fix：完善feign与网关示例

---
+++ microservice-alibaba/nacos-feign/src/main/java/xyz/ibudai/NacosFeignApplication.java
@@ -0,0 +1,17 @@
+package xyz.ibudai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients(basePackages = "xyz.ibudai.service")
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosFeignApplication.class, args);
+    }
+
+}
---
+++ microservice-alibaba/nacos-gateway/src/main/java/xyz/ibudai/NacosGatewayApplication.java
@@ -0,0 +1,17 @@
+package xyz.ibudai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosGatewayApplication.class, args);
+    }
+
+}
